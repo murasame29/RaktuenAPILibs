88bf152781c3f49a48e1607a36bf8753fbbe0420
murasame <oogiriminister@gmail.com>
2023-04-03 22:27:10 +0900

    add:first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module server
+
+go 1.19
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"server/rakutenapi"
+)
+
+func main() {
+	body, err := rakutenapi.InitRequest().RakutenSearch.Get().Keyword("golang").Hits(1).GenreInformationFlag(false).Do()
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(body.Items[0])
+}
